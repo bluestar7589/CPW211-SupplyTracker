3b56f600695fd8ed18caf55458241d9fe4488134
bluestar7589 <bluestar7589@yahoo.com>
2024-07-25 17:14:33 -0700

    Created the mainscreen form and basic menu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menuStrip1 = new MenuStrip();
+            productsToolStripMenuItem = new ToolStripMenuItem();
+            vendorsToolStripMenuItem = new ToolStripMenuItem();
+            ordersToolStripMenuItem = new ToolStripMenuItem();
+            departmentsToolStripMenuItem = new ToolStripMenuItem();
+            usersToolStripMenuItem = new ToolStripMenuItem();
+            userManagementToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { productsToolStripMenuItem, vendorsToolStripMenuItem, ordersToolStripMenuItem, departmentsToolStripMenuItem, usersToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // productsToolStripMenuItem
+            // 
+            productsToolStripMenuItem.Name = "productsToolStripMenuItem";
+            productsToolStripMenuItem.Size = new Size(66, 20);
+            productsToolStripMenuItem.Text = "Products";
+            // 
+            // vendorsToolStripMenuItem
+            // 
+            vendorsToolStripMenuItem.Name = "vendorsToolStripMenuItem";
+            vendorsToolStripMenuItem.Size = new Size(61, 20);
+            vendorsToolStripMenuItem.Text = "Vendors";
+            // 
+            // ordersToolStripMenuItem
+            // 
+            ordersToolStripMenuItem.Name = "ordersToolStripMenuItem";
+            ordersToolStripMenuItem.Size = new Size(54, 20);
+            ordersToolStripMenuItem.Text = "Orders";
+            // 
+            // departmentsToolStripMenuItem
+            // 
+            departmentsToolStripMenuItem.Name = "departmentsToolStripMenuItem";
+            departmentsToolStripMenuItem.Size = new Size(87, 20);
+            departmentsToolStripMenuItem.Text = "Departments";
+            // 
+            // usersToolStripMenuItem
+            // 
+            usersToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { userManagementToolStripMenuItem });
+            usersToolStripMenuItem.Name = "usersToolStripMenuItem";
+            usersToolStripMenuItem.Size = new Size(47, 20);
+            usersToolStripMenuItem.Text = "Users";
+            // 
+            // userManagementToolStripMenuItem
+            // 
+            userManagementToolStripMenuItem.Name = "userManagementToolStripMenuItem";
+            userManagementToolStripMenuItem.Size = new Size(180, 22);
+            userManagementToolStripMenuItem.Text = "User Management";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Dashboard";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem productsToolStripMenuItem;
+        private ToolStripMenuItem vendorsToolStripMenuItem;
+        private ToolStripMenuItem ordersToolStripMenuItem;
+        private ToolStripMenuItem departmentsToolStripMenuItem;
+        private ToolStripMenuItem usersToolStripMenuItem;
+        private ToolStripMenuItem userManagementToolStripMenuItem;
     }
 }
