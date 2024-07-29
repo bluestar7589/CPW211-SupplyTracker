34b226f838dbf61240c644a940989bc5850c7652
bluestar7589 <bluestar7589@yahoo.com>
2024-07-29 01:02:53 -0700

    Added function GetAllVendor to support  for Product model

---
+++ Databases/VendorDB.cs
@@ -0,0 +1,24 @@
+﻿using SupplyTracker.Data;
+using SupplyTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyTracker.Databases
+{
+    internal class VendorDB
+    {
+        /// <summary>
+        /// This method to get all the vendor from database
+        /// </summary>
+        /// <returns></returns>
+        public static List<Vendor> GetAllVendors()
+        {
+            using SupplyTrackerContext context = new SupplyTrackerContext();
+            List<Vendor> lstVendors = (from v in context.Vendors select v).ToList();
+            return lstVendors;
+        }
+    }
+}
